e9b93761360ff1bcbfdeb79c396bcf6189f098a5
KimTaeyoun <kimty1121@gmail.com>
2022-10-01 02:31:02 +0900

    타입스크립트 기초 - 변수와 함수 타입 정의하기

---
+++ getting-started/class-note/1_type_basic.ts
@@ -0,0 +1,30 @@
+// js 문자열 선언
+// var str = 'hello';
+
+// ts 문자열 선언
+let str: string = 'hello';
+
+let num: number = 10;
+
+// 배열
+let arr: Array<number> = [1,2,3];
+let heroes: Array<String> = ['Cap', 'Thor', 'Hulk'];
+let item: number[] = [1,2,3];
+
+// ts 튜플
+let addrss: [string, number] = ['gangnam', 10];
+
+// ts 객체
+let obj: object = {};
+let person: object = {
+  name: 'capt',
+  age: 100
+}
+
+let typed_person: { name: string, age: number } = {
+  name: 'thor',
+  age: 2000
+}
+
+// ts 진위값
+let show: boolean = true;
---
+++ getting-started/class-note/2_functions.ts
@@ -0,0 +1,22 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a: number, b: number) {
+  return a + b;
+}
+sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+  return 10;
+}
+
+function sum2(a: number, b: number): number {
+  return a + b;
+}
+sum2(10, 20);
+
+// 함수의 옵셔널 파라미터
+function pring_log(a: string, b?: string) {
+
+}
+pring_log('hello world');
+pring_log('hello ts', 'abc')
